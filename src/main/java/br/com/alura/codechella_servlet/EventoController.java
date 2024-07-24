2e894bacd43e0aa5b44657b9fe1ff896708eb97b
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-07-24 19:01:43 -0300

    Inclusão de testes

--- src/main/java/br/com/alura/codechella_servlet/EventoController.java
+++ src/main/java/br/com/alura/codechella_servlet/EventoController.java
@@ -1,6 +1,7 @@
 package br.com.alura.codechella_servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -41,6 +43,7 @@ public class EventoController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EventoDto cadastrar(@RequestBody EventoDto dto) {
         return servico.cadastrar(dto);
     }
--- src/test/java/br/com/alura/codechella_servlet/CodechellaServletApplicationTests.java
+++ src/test/java/br/com/alura/codechella_servlet/CodechellaServletApplicationTests.java
@@ -1,13 +1,57 @@
 package br.com.alura.codechella_servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import java.time.LocalDate;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CodechellaServletApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
+	void cadastraNovoEvento() throws Exception {
+		EventoDto dto = new EventoDto(null, TipoEvento.SHOW, "Kiss",
+				LocalDate.parse("2025-01-01"), "Show da melhor banda que existe");
+
+		mockMvc.perform(post("/eventos")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(dto)))
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.id").isNotEmpty())
+				.andExpect(jsonPath("$.tipo").value(dto.tipo().toString()))
+				.andExpect(jsonPath("$.nome").value(dto.nome()))
+				.andExpect(jsonPath("$.data").value(dto.data().toString()))
+				.andExpect(jsonPath("$.descricao").value(dto.descricao()));
+	}
+
+	@Test
+	void buscarEvento() throws Exception {
+		EventoDto dto = new EventoDto(13L, TipoEvento.SHOW, "The Weeknd",
+				LocalDate.parse("2025-11-02"), "Um show eletrizante ao ar livre com muitos efeitos especiais.");
+
+		mockMvc.perform(get("/eventos"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[12].id").value(dto.id()))
+				.andExpect(jsonPath("$[12].tipo").value(dto.tipo().toString()))
+				.andExpect(jsonPath("$[12].nome").value(dto.nome()))
+				.andExpect(jsonPath("$[12].data").value(dto.data().toString()))
+				.andExpect(jsonPath("$[12].descricao").value(dto.descricao()));
 	}
 
 }
